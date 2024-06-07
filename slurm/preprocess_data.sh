a3546d10bf6e0fce1425b246a961b4ed4fcd74f5
Nathan Fradet <56734983+Natooz@users.noreply.github.com>
2024-06-07 15:03:34 +0200

    adding slurm files

---
+++ slurm/preprocess_data.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set SLURM / hardware environment
+#SBATCH --job-name=preprocess-data
+#SBATCH --output=logs/preprocess-data.out
+#SBATCH --error=logs/preprocess-data_err.out
+#SBATCH --account=def-pasquier
+#SBATCH --mail-user=raa60@sfu.ca # Default mail
+#SBATCH --nodes=1            # total nb of nodes
+#SBATCH --ntasks-per-node=1  # nb of tasks per node
+#SBATCH --cpus-per-task=5    # nb of CPU cores per task
+#SBATCH --time=15:00:00
+
+# Output ram info
+echo "START TIME: $(date)"
+free -m
+
+# Defining the right environment variables
+export PYTHONPATH=$WORK/MMM
+export HF_HOME=$SCRATCH/.hf_cache
+
+# Load the python environment
+# Make sure the required packages are installed
+module load python/3.9.12
+
+# Run the training
+srun --jobid "$SLURM_JOBID" bash -c "python3.9 clean_mmd.py"
+srun --jobid "$SLURM_JOBID" bash -c "python3.9 tokenize_datasets.py"
+srun --jobid "$SLURM_JOBID" bash -c "python3.9 gather_tokens_paths_per_genre.py"
+
+echo "END TIME: $(date)"
---
+++ slurm/train_model.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Inspired from https://github.com/bigscience-workshop/bigscience/blob/7ccf7e42577fe71e88cf8bed3b9ca965c7afb8f7/train/tr11-176B-ml/tr11-176B-ml.slurm
+
+# Set SLURM / hardware environment
+#SBATCH --job-name=train-smile-500m
+#SBATCH --output=logs/train-smile-500m.out
+#SBATCH --error=logs/train-smile-500m_err.out
+#SBATCH --account=def-pasquier
+#SBATCH --mail-user=raa60@sfu.ca # Default mail
+#SBATCH --nodes=1            # total nb of nodes
+#SBATCH --ntasks-per-node=1  # nb of tasks per node
+#SBATCH --gpus-per-node=v100l:4
+#SBATCH --cpus-per-task=64   # nb of CPU cores per task
+#SBATCH --time=20:00:00      # max for A100 is 20h
+
+# Define model size (500,030,400 params)
+MODEL_TRAIN_ARGS=" \
+    --deepspeed \
+    --no-torch-compile \
+    --per-device-batch-size 8 \
+    --per-device-batch-size-test 16 \
+    "
+
+# Output GPUs and ram info
+echo "START TIME: $(date)"
+nvidia-smi
+nvidia-smi topo -m
+free -m
+
+# Hardware vars
+GPUS_PER_NODE=8
+MASTER_ADDR=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+MASTER_PORT=9902
+echo "Master addr: $MASTER_ADDR"
+echo "Node list: $SLURM_JOB_NODELIST"
+
+# Defining the right environment variables
+export PYTHONPATH=$WORK/MMM
+export HF_HOME=$SCRATCH/.hf_cache
+#export HF_METRICS_OFFLINE=1
+#export TRANSFORMERS_OFFLINE=1
+#export OMP_NUM_THREADS=1
+
+# Set launcher command with params
+export LAUNCHER="torchrun \
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $SLURM_NNODES \
+    --node_rank $SLURM_PROCID \
+    --rdzv_endpoint $MASTER_ADDR:$MASTER_PORT \
+    --rdzv_backend c10d \
+    --max_restarts 0 \
+    --role $SLURMD_NODENAME: \
+    --tee 3 \
+    "
+
+# Load the python environment
+source ../venv/bin/activate
+# pip install -U flash-attn --no-build-isolation
+
+# Run the training
+srun --jobid "$SLURM_JOBID" bash -c "$LAUNCHER exp_generation.py $MODEL_TRAIN_ARGS"
+
+echo "END TIME: $(date)"
