4540100721aa35209e04533b1065d20037b0b26f
Nathan Fradet <56734983+Natooz@users.noreply.github.com>
2024-08-30 14:45:54 +0200

    setting memory to 40GB

--- slurm/train_t5.sh
+++ slurm/train_t5.sh
@@ -12,7 +12,7 @@
 #SBATCH --ntasks-per-node=1  # nb of tasks per node
 #SBATCH --gpus-per-node=a100:4
 #SBATCH --cpus-per-task=4   # nb of CPU cores per task
-#SBATCH --mem=32G
+#SBATCH --mem=40G
 #SBATCH --time=24:00:00
 
 # Define args
