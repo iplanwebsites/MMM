a39f4e9aadabcbf6fc978307aa622719374b438d
Davide <davide.rizzotti@mail.polimi.it>
2024-09-16 15:04:24 +0200

    dummy model ready

--- mmm/inference.py
+++ mmm/inference.py
@@ -99,7 +99,6 @@ def generate_new_track(
     output_seq = TokSequence(ids=output_ids[0].tolist(), are_ids_encoded=True)
 
     # Remove attribute controls from the sequence
-    # TODO: TEST this line
     output_seq = (
         output_seq[: len(input_seq)] + output_seq[len(input_seq) + len(track[1]) :]
     )
--- tests/utils_tests.py
+++ tests/utils_tests.py
@@ -7,6 +7,8 @@ from miditok import MMM
 from symusic import Score
 from torch import LongTensor
 
+import numpy as np
+
 HERE = Path(__file__).parent
 MIDI_PATHS = list((HERE / "midis").glob("**/*.mid"))
 MIDI_PATH = HERE / "midis" / "I Gotta Feeling.mid"
@@ -18,9 +20,24 @@ class DummyModel:
     def __init__(self, tokenizer: MMM) -> None:
         self.tokenizer = tokenizer
         self._bar_infilling_token_id = self.tokenizer["Infill_Bar"]
+        self._bar_none_token_id = self.tokenizer["Bar_None"]
         score = Score(MIDI_PATH)
         # TODO extract the desired portion from the score tokenize it and save attribute
-        self.token_ids = token_ids
+
+        kwargs={}
+        if type(self.tokenizer).__name__ == "MMM":
+            kwargs["concatenate_track_sequences"] = False
+
+        tokens = tokenizer(score, **kwargs)
+
+        tokenizer.decode_token_ids(tokens[0])
+        bar_none_tokens = np.where(np.array(tokens[0].ids) == self._bar_none_token_id)[0]
+        # Get 4 bars of content for the infilling part
+        self.infill_generated_tokens = tokens[0].ids[bar_none_tokens[0]:bar_none_tokens[4]]
+        self.infill_generated_tokens.append(tokenizer.vocab["FillBar_End"])
+
+        self.track_generated_content = tokens[0].ids
+
 
     def generate(self, token_ids: LongTensor) -> LongTensor:
         """
@@ -31,7 +48,7 @@ class DummyModel:
         """
         # If we have to infill bars
         if torch.any(token_ids == self._bar_infilling_token_id):
-            generated_tokens = []
+            generated_tokens = self.infill_generated_tokens
             token_ids = torch.cat(
                 (
                     token_ids,
@@ -42,7 +59,7 @@ class DummyModel:
 
         # otherwise, we are generating a new track
         else:
-            generated_tokens = []
+            generated_tokens = self.track_generated_content
             token_ids = torch.cat(
                 (
                     token_ids,
